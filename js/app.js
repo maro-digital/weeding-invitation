3db12e1156a60fc42544ae9c8d5cf472d2ad9033
maro-digital <marostoreofficial@gmail.com>
2024-05-02 00:59:26 +0800

    updates

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-import { util } from './util.js';
-import { like } from './like.js';
-import { theme } from './theme.js';
-import { audio } from './audio.js';
-import { comment } from './comment.js';
-import { progress } from './progress.js';
-import { pagination } from './pagination.js';
+import { util } from "./util.js";
+import { like } from "./like.js";
+import { theme } from "./theme.js";
+import { audio } from "./audio.js";
+import { comment } from "./comment.js";
+import { progress } from "./progress.js";
+import { pagination } from "./pagination.js";
 
 window.util = util;
 window.like = like;
